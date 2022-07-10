d5121b677c5cfadf46776b23b5f12eaa541ad9e5
munyasia1996 <munyasiagerald@gmail.com>
2022-07-10 12:39:10 -0700

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,19 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * _isupper - checks for uppercase character.
- * @c: variable to check.
+ * _isupper - checks for uppercase characters.
+ * @c: The character to be checked.
  *
- * Return: 1 if @c is uppercase, 0 if not.
+ * Return: 1 if character is uppercase, 0 otherwise.
  */
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File - main.h
+ * Author: Genius Villagers
+ * Desc: Header contains protypes for all functions
+ *      used in 0x04-more_functions_nested_loops directory
+ */
+
+int _putchar(char c);
+
+
+
+
+
+
+#endif
